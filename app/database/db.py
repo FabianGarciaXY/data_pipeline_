d48bd8efe1046a8cc988b5099e6a9d200f99af96
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-06-24 23:39:05 -0500

    Configured the database connection in db.js

---
+++ app/database/db.py
@@ -0,0 +1,21 @@
+# Importing os and connect function to connect to database
+import os
+from psycopg2 import connect
+
+
+# Excuting connection
+def connect_db():
+    try:
+        # Getting credentials from docker-compose environment variables
+        host = os.environ['PG_HOST']
+        port = os.environ['PG_PORT']
+        db = os.environ['PG_DATABASE']
+        user = os.environ['PG_USER']
+        password = os.environ['PG_PASSWORD']
+
+        # Connecting to the database
+        connection = connect(dbname=db, user=user, password=password, host=host, port=port)
+        return connection
+
+    except Exception as ex:
+        raise ex
