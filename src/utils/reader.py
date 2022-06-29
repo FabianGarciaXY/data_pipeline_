7363ebe7558413fb99f6faf657832fc170fc4218
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-06-29 03:43:36 -0500

    Update get_location function to catch non founded locations

--- src/utils/reader.py
+++ src/utils/reader.py
@@ -1,13 +1,33 @@
 import requests
 import os
 
-# Fething vehicle data from the API
+
+# Function for fething vehicle data from the API
+# @return a list of objects containing the vehicles dada
 def fetch_vehicles_data():
-
     try:
-        url = os.environ['VEHICLES_API']
-    except: 
-        url = 'https://datos.cdmx.gob.mx/api/3/action/datastore_search?resource_id=ad360a0e-b42f-482c-af12-1fd72140032e&limit=1000'
+        vehicles_api = os.environ['VEHICLES_API']
+        response = requests.get(vehicles_api).json() 
+        data = response['result']['records']  
+    except Exception as err:
+        raise err
 
-    response = requests.get(url)
-    return response.json()
+    return data
+
+
+# Function for get address of th vehicles
+# @param: [String] receives a string containing the latitude and longitude of the vehicle
+# @return: [String] returns a string containing the delegation of the vehicle
+def get_location(coords):
+    try:
+        geocoder_api = os.environ['GEOCODER_API']
+        api_key = os.environ['GEOCODER_API_KEY']
+        # Calling the API
+        response = requests.get(f'{geocoder_api}latlng={coords}&result_type=route&key={api_key}').json()
+        # Getting the specific address of the vehicle
+        address = response['results'][0]['formatted_address'].split(', ')
+        delegation = address[len(address) -4].lower()
+        return delegation
+
+    except:
+        return 'Not available'
