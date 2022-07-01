8ebcfc9bd90dde4f106aa034e25f14af4afd2e7b
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-07-01 16:28:58 -0500

    Updated parameters in delegections controller

--- src/api/controllers/delegations.py
+++ src/api/controllers/delegations.py
@@ -1,17 +1,20 @@
+import api.services.delegation as service
+
 # This module contains all functions to controll requests
 
 # @description: Function to handle /delegations requests
 # @return: the response from the service
 def get_available_delegations():
-    data = {"test": "test"}
-    return data
+    service_response = service.get_delegations()
+    return service_response
+
 
 # @description: Function to control data received from /delegations/<name> requests
 # @return: the response from the service
-def get_vehicle_by_id(name):
-    name = name
-
-    if name == None or not name.isalpha():
-        return {"error": "Invalid id"}
+def get_vehicles_by_delegation(name):
+    if len(name) < 1 or isinstance(name, str) is False:
+        service_response = {'error' : 'enter a valid name' }
     else:
-        return # function call
+        service_response = service.get_vehicles(name)
+        
+    return service_response
