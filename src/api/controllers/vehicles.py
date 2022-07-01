4918918c07cdcfa880eb2974a212f862e5b4e2fc
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-06-30 20:01:59 -0500

    Imported vehicle service module

--- src/api/controllers/vehicles.py
+++ src/api/controllers/vehicles.py
@@ -1,17 +1,19 @@
+import api.services.vehicles as service
+
 # This module contains all functions to controll requests
 
 # @description: Function to handle /metrobuses requests
 # @return: the response from the service
-def get_all_vehicles():
-    data = {"test": "test"}
-    return data
+def get_available_vehicles():    
+    service_response = service.get_vehicles()
+    return service_response 
+
 
 # @description: Function to control data received from /metrobuses/<id> requests
 # @return: the response from the service
-def get_vehicle_by_id(id):
-    data = id
-
-    if id < 0 or type(id) != int:
-        return {"error": "Invalid id"}
+def get_vehicle_address_by_id(id):
+    if type(id) == 'str' or id < 0:
+        return 'Not found, invalid id'
     else:
-        return # function call
+        service_response = service.get_vehicle(id)
+        return service_response
