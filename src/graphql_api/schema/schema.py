dc763f5df28d181e8aaa7f61de9bce77bf29f6a4
FabianGarciaXY <fabyan_0798@hotmail.com>
2023-01-21 18:12:46 -0600

    Updated tasks.sh, schema and graphql

--- src/graphql_api/schema/schema.py
+++ src/graphql_api/schema/schema.py
@@ -1,6 +1,6 @@
 import os
 from ariadne import make_executable_schema, load_schema_from_path, snake_case_fallback_resolvers
-from graphql_api.schema.resolvers.vehicles_resolver import query
+from graphql_api.schema.resolvers.vehicles_resolver import query, error
 
 
 # Getting the schema
@@ -8,5 +8,5 @@ type_defs = load_schema_from_path("{}/src/graphql_api/schema/".format(os.getcwd(
 
 # Creating the executable schema
 schema = make_executable_schema(
-    type_defs, query, snake_case_fallback_resolvers
+    type_defs, [query, error], snake_case_fallback_resolvers
 )
